48a0b94b418fc8c1ad9e324b85736af271604e8b
Umesh-daiict <umeshsavaliya007@gmail.com>
2024-11-25 15:14:51 +0000

    init

---
+++ src/main/java/sun/dev/ijobs/IjobsApplication.java
@@ -0,0 +1,13 @@
+package sun.dev.ijobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sun/dev/ijobs/IjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package sun.dev.ijobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
