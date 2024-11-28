9115ae0bd82ab40859cbe7a9d9866fe6f2f3f03f
Umesh-daiict <umeshsavaliya007@gmail.com>
2024-11-28 17:33:36 +0000

    day 3

--- src/main/java/sun/dev/ijobs/Ijob.java
+++ src/main/java/sun/dev/ijobs/Ijob.java
@@ -1,4 +1,30 @@
 package sun.dev.ijobs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Document(collation = "jobs")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ijob {
+    @Id
+    private ObjectId id;
+    private String type;
+    private Number var_type;
+    private Date data;
+    private Number version;
+    private String country_code;
+    private Boolean is_first;
+    private Number _salary;
+    private Number _year_exp;
+    private List<String> _past_job_type;
+    private String _company;
 }
---
+++ src/main/java/sun/dev/ijobs/User.java
@@ -0,0 +1,16 @@
+package sun.dev.ijobs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private ObjectId id;
+    private String name;
+}
